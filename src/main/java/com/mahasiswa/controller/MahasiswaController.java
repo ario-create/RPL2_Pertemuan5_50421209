c0364a389a10502406352dfa3c773876b5dc0e7b
ario-create <ario3533@student.gunadarma.ac.id>
2024-11-27 21:50:47 +0700

    Pertemuan 5

---
+++ src/main/java/com/mahasiswa/controller/MahasiswaController.java
@@ -0,0 +1,79 @@
+package com.mahasiswa.controller;
+import com.mahasiswa.model.ModelMahasiswa;
+import com.mahasiswa.repository.MahasiswaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import java.util.List;
+import java.util.Scanner;
+
+@Controller
+public class MahasiswaController {
+       
+    
+    @Autowired
+	private MahasiswaRepository mahasiswaRepository;
+	
+	public void tampilkanMenu() {
+		Scanner scanner = new Scanner(System.in);
+		int opsi;
+
+		do {
+			System.out.println("\nMenu: ");
+			System.out.println("1. Tampilkan semua mahasiswa");
+			System.out.println("2. Tambah mahaiswa baru");
+			System.out.println("3. Cek koneksi database");
+			System.out.println("4. Keluar");
+			System.out.println("Pilih Opsi: ");
+			opsi = scanner.nextInt();
+			scanner.nextLine();
+
+			switch (opsi) {
+				case 1:
+					tampilkanSemuaMahasiswa();
+					break;
+				case 2:
+					tambahMahasiswa(scanner);
+					break;
+				case 3:
+					cekKoneksi();
+					break;
+				case 4:
+					System.out.println("Keluar dari program");
+					break;
+				default:
+					System.out.println("Opsi tidak valid, coba lagi");
+			}
+		} while (opsi != 4);
+	}
+
+	private void tampilkanSemuaMahasiswa() {
+		List<ModelMahasiswa> mahasiswaList = mahasiswaRepository.findAll();
+		if (mahasiswaList.isEmpty()) {
+			System.out.println("Tidak ada data mahasiswa.");
+		} else {
+			mahasiswaList.forEach(mahasiswa -> System.out.println(mahasiswa));
+		}
+	}
+
+	private void tambahMahasiswa(Scanner scanner) {
+		System.out.println("Masukkan NPM: ");
+		String npm = scanner.nextLine();
+		System.out.println("Masukkan Nama: ");
+		String nama = scanner.nextLine();
+		System.out.println("Masukkan Semester: ");
+		String semester = scanner.nextLine();
+		System.out.println("Masukkan ipk: ");
+		String ipk = scanner.nextLine();
+	}
+
+	private void cekKoneksi() {
+		try {
+			mahasiswaRepository.findAll();
+			System.out.println("Koneksi ke database berhasil");
+		} catch (Exception e) {
+			System.out.println("Gagal terhubung ke database");
+		}
+	}
+}
+
